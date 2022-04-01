c302d8064142e721c4a11aefcc341161bf314e63
rafa <r.loboplaza@hotmail.com>
2022-04-01 12:31:36 -0300

    paginacion lista

--- controllers/getExternalData.js
+++ controllers/getExternalData.js
@@ -20,6 +20,13 @@ const getPosts = async(req,res)=>{
 }
 
 const getPhotos = async(req,res)=>{
+    const limit = parseInt(req.query.limit)
+    const page = parseInt(req.query.page)
+
+    const startIndex = (page-1)*limit
+    const endIndex = page*limit
+
+
     const requestOptions = {
         method: "GET",
         url: 'https://jsonplaceholder.typicode.com/photos',        
@@ -27,7 +34,14 @@ const getPhotos = async(req,res)=>{
     try {
         let response = await axios(requestOptions)
         if(response){
-           return res.status(200).json(response.data)
+            const total = response.data.length
+            const resPaginated = {
+                page: page,
+                next: page*limit >= total ? null : page+1,
+                previous: startIndex === 0 ? null : page-1 ,
+                data: response.data.slice(startIndex, endIndex)
+            }
+           return res.status(200).json(resPaginated)
         }
     } catch (error) {
         console.log('err', error)
